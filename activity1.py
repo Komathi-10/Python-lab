2d44f7d682f2a7bb12ed411d85794bbd145a5b11
Komathi-10 <komathigopi10@gmail.com>
2025-03-25 19:05:03 +0530

    activity1.py

--- activity1.py
+++ activity1.py
@@ -1,6 +1,8 @@
 
 
-#1.calculate the multiplication and sum of two numbers
+#1.
+print("\nCalculate the multiplication and sum of two numbers")
+
 a=int(input("Enter first number:"))
 b=int(input("Enter second number:"))
 mul=a*b
@@ -9,15 +11,16 @@ print("Multiplication of 2 numbers:",mul)
 print("Sum of 2 numbers:",sum) 
 
 
-#2.declare two variables and
-#print that which variable is largest using ternary operators
+#2.
+print("\n\nDeclare two variables and print that which variable is largest using ternary operators")
 a = 10
 b = 20
 largest = a if a > b else b
 print("The largest number is:",largest)
 
 
-#3.take input from user (name,age,city) and print it in formatted string
+#3.
+print("\n\nTake input from user (name,age,city) and print it in formatted string")
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 city = input("Enter your city: ")
@@ -26,21 +29,24 @@ print(f"Age is {age}")
 print(f"City is {city}") 
 
 
-#4.find area of a triangle whose sides sre given
+#4.
+print("\n\nfind area of a triangle whose sides sre given")
 b=int(input("Enter the breadth:"))
 h=int(input("Enter the height:"))
 area=0.5*b*h
 print("Area of triangle is:",area)
 
 
-#5.check whether a number is even or odd
+#5.
+print("\n\nCheck whether a number is even or odd")
 num=int(input("ENTER THE NUMBER:"))
 if num%2==0:
     print("Even number")
 else: print("Odd number")  
 
 
-#6.find the square and cube of a given number
+#6.
+print("\n\nFind the square and cube of a given number")
 n=int(input("ENTER THE NUMBER:"))
 square=n*n
 cube=n*n*n
