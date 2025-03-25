65a1153e54dbe5ada087904883ad8e413ab402aa
Komathi-10 <komathigopi10@gmail.com>
2025-03-25 18:52:45 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,56 @@
+
+
+#1.calculate the multiplication and sum of two numbers
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+mul=a*b
+sum=a+b
+print("Multiplication of 2 numbers:",mul)
+print("Sum of 2 numbers:",sum) 
+
+
+#2.declare two variables and
+#print that which variable is largest using ternary operators
+a = 10
+b = 20
+largest = a if a > b else b
+print("The largest number is:",largest)
+
+
+#3.take input from user (name,age,city) and print it in formatted string
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+print(f"\nName is {name}")
+print(f"Age is {age}")
+print(f"City is {city}") 
+
+
+#4.find area of a triangle whose sides sre given
+b=int(input("Enter the breadth:"))
+h=int(input("Enter the height:"))
+area=0.5*b*h
+print("Area of triangle is:",area)
+
+
+#5.check whether a number is even or odd
+num=int(input("ENTER THE NUMBER:"))
+if num%2==0:
+    print("Even number")
+else: print("Odd number")  
+
+
+#6.find the square and cube of a given number
+n=int(input("ENTER THE NUMBER:"))
+square=n*n
+cube=n*n*n
+print("Square root of the given numbers:",square)
+print("Cube root of the given5 numbers:",cube)  
+
+
+
+
+
+
+
+   
